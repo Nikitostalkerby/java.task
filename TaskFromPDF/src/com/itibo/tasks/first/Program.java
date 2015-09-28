18a35c2c95f006fdc2e3b968076bfe25b3140983
Kryvitski Nikita <hax2033@gmail.com>
2015-09-28 18:36:37 +0400

    Программа выдает то, что требуется по условию.
    Выдает символы в обратном порядке по количеству раз использования.
    Если я чего-то напутал с условиями(обратная сортировка или нет), это
    исправляется всего одним выражением Collections.reverseOrder();

--- TaskFromPDF/src/com/itibo/tasks/first/Program.java
+++ TaskFromPDF/src/com/itibo/tasks/first/Program.java
@@ -3,12 +3,13 @@ package com.itibo.tasks.first;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 /*
- * Описание зачем оно надо
+ * Можно разбить еще на пару классов. Можно написать интерфейс. Но нет.
  */
 
 public class Program {
@@ -17,20 +18,25 @@ public class Program {
 
 		System.out.println("Enter the line: ");
 
+		// enter the line
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
+		// get lowercase char array from string
 		char[] line = str.toLowerCase().toCharArray();
 
 		char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
-
+		
+		// dictionary for saving key/value pair
 		Map<Character, Integer> dictionary = new HashMap<Character, Integer>();
 
 		System.out.println("Vowels: ");
 
+		// knowing all vowel chars
 		for (int i = 0; i < line.length; i++) {
 			for (int k = 0; k < vowels.length; k++) {
 				if (line[i] == vowels[k]) {
 					if (dictionary.containsKey(line[i])) {
+						// and saving to map
 						Integer value = (Integer) dictionary.get(line[i]);
 						dictionary.put(line[i], value + 1);
 					} else {
@@ -47,15 +53,20 @@ public class Program {
 		
 		System.out.println("");
 		
-		ValueComparator bvc = new ValueComparator(dictionary);
-		TreeMap<Character, Integer> sorted_dictionary = new TreeMap<Character, Integer>(bvc);
+		// using class to saving similar chars
+		int index = dictionary.size();
+		CharInt[] ci = new CharInt[index];
+		index = 0;
 		
-		sorted_dictionary.putAll(dictionary);
+		for(Map.Entry<Character, Integer>entry:dictionary.entrySet()) {
+			ci[index++] = new CharInt(entry.getKey(), entry.getValue());
+		}
 		
-		System.out.println("results:");
-		
-		for (Character key : sorted_dictionary.keySet()) {
-			System.out.println(key + ":" + sorted_dictionary.get(key));
+		// lazy revers sorting
+		Arrays.sort(ci, Collections.reverseOrder());
+		System.out.println("Sorted map");
+		for (CharInt cin:ci) {
+			System.out.println(cin.c + ": " + cin.i);
 		}
 	}
 }
