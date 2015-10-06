7745b4af0d371a1ceef3af964e20b0dce047bad7
Kryvitski Nikita <hax2033@gmail.com>
2015-10-06 12:17:15 +0400

    Houston, we have a problem.
    Wrong Instruction.

--- TaskFromPDF/src/com/itibo/tasks/second/Instruction.java
+++ TaskFromPDF/src/com/itibo/tasks/second/Instruction.java
@@ -7,6 +7,21 @@ public class Instruction {
 		System.out.println("Instruction begin. \n");
 		
 		System.out.println(first.getSpace() + second.getSpace());
+		
+		int a = first.getWater();
+		int b = second.getWater();
+		
+		first.setWater(first.getSpace());
+		first.setIsFull(true);
+		
+		second.setWater(first.getSpace());
+		
+		while(second.getIsFull() == false) {
+			if(second.getSpace() == b) {
+				second.setIsFull(true);
+			}
+			b += a;
+		}
 	}
 
 }
