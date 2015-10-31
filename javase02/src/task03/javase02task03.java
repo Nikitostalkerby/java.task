a5d3f43054ae4d254d60aefd965b87aa745ae4ae
Nikitos Krivitski <hax2033@gmail.com>
2015-11-01 00:46:43 +0300

    Introducing to regexp

--- javase02/src/task03/javase02task03.java
+++ javase02/src/task03/javase02task03.java
@@ -1,17 +1,35 @@
 package task03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
- * Created by Vaas on 31.10.2015.
+ * Created by union.one on 31.10.2015.
  */
 public class javase02task03 {
 
     public static void main(String[] args) {
 
-        String date = "yyyy/mm/dd";
-        date.replaceAll("-","/");
-        String year = new String(date.substring(0,4));
-        String month = new String(date.substring(5,7));
-        String day = new String(date.substring(8,10));
-        System.out.println(year + " " + month + " " + day);
+        String date = null;
+
+        System.out.println("Enter the date: ");
+
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            date = bufferedReader.readLine();
+        } catch(IOException e) {
+            System.out.println("Something is wrong: " + e.toString());
+        }
+
+        Pattern pattern = Pattern.compile(":|;|;;|/|//|::|\\.");
+        String [] list = pattern.split(date);
+
+        System.out.println("Your date is: ");
+        Arrays.asList(list).forEach(str -> System.out.println(str + " "));
+
+        //System.out.println("Your date is: " + date);
     }
 }
