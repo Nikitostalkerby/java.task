b8ba3ad40396602e2f843fa668d0bb23b897dfa4
Nikitos Krivitski <hax2033@gmail.com>
2015-10-31 19:15:55 +0300

    Introducing to parsing

--- javase02/src/task03/javase02task03.java
+++ javase02/src/task03/javase02task03.java
@@ -4,4 +4,14 @@ package task03;
  * Created by Vaas on 31.10.2015.
  */
 public class javase02task03 {
+
+    public static void main(String[] args) {
+
+        String date = "yyyy/mm/dd";
+        date.replaceAll("-","/");
+        String year = new String(date.substring(0,4));
+        String month = new String(date.substring(5,7));
+        String day = new String(date.substring(8,10));
+        System.out.println(year + " " + month + " " + day);
+    }
 }
