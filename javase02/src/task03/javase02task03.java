3887598e6820482999b5bd3614765a9965d0c2f8
Nikitos Krivitski <hax2033@gmail.com>
2015-11-01 17:10:26 +0300

    Closely to solution

--- javase02/src/task03/javase02task03.java
+++ javase02/src/task03/javase02task03.java
@@ -3,8 +3,8 @@ package task03;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.regex.Matcher;
+import java.util.*;
+
 import java.util.regex.Pattern;
 
 /**
@@ -15,21 +15,68 @@ public class javase02task03 {
     public static void main(String[] args) {
 
         String date = null;
+        final String COMMA_PATTERN = ":|;|;;|/|//|::|\\.|-";
+        final String DATE_PATTERN = "[0-9]\\S";
+        final String MM = "mm";
+        final String DD = "dd";
+        final String YYYY = "yyyy";
+        final String DDMM = "ddmm";
+
 
         System.out.println("Enter the date: ");
 
         try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
             date = bufferedReader.readLine();
         } catch(IOException e) {
-            System.out.println("Something is wrong: " + e.toString());
+            System.out.println("IOException: " + e.toString());
         }
 
-        Pattern pattern = Pattern.compile(":|;|;;|/|//|::|\\.");
-        String [] list = pattern.split(date);
+        try{
+            date = date.replaceAll(" ", "");
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException: " + e.toString());
+        }
 
-        System.out.println("Your date is: ");
-        Arrays.asList(list).forEach(str -> System.out.println(str + " "));
 
-        //System.out.println("Your date is: " + date);
+        Pattern commaPattern = Pattern.compile(COMMA_PATTERN);
+        Pattern datePattern = Pattern.compile(DATE_PATTERN);
+
+        List<String> dateList = Arrays.asList(commaPattern.split(date));
+        Map<Integer, String> map = new HashMap<>();
+
+        for(String i : dateList) {
+            if(i.length() == 2) {
+                if(Integer.parseInt(i) >= 0 && Integer.parseInt(i) <= 12) {
+                    if (map.containsKey(Integer.parseInt(i))) {
+                        // костыль от отца. потом не забыть поделить на 10
+                        map.put(Integer.parseInt(i+0), MM);
+                    } else {
+                        map.put(Integer.parseInt(i), DD);
+                    }
+                    //map.put(Integer.parseInt(i), DDMM);
+                }else if (Integer.parseInt(i) >= 0 && Integer.parseInt(i) <= 31)  {
+                    map.put(Integer.parseInt(i), DD);
+                }
+                else if(Integer.parseInt(i) < 0 && Integer.parseInt(i) > 31) {
+                    System.out.println("Incorrect format. Break this.");
+                    break;
+                }
+            } else if (i.length() == 4) {
+                if(Integer.parseInt(i) >= 0) {
+                    map.put(Integer.parseInt(i), YYYY);
+                }
+            }
+        }
+
+        List<String> commaList = Arrays.asList(datePattern.split(date));
+
+
+        System.out.println("\nYour date is: ");
+        Arrays.asList(dateList).forEach(str -> System.out.println(str));
+        Arrays.asList(commaList).forEach(str -> System.out.println(str));
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + "<->" + entry.getValue());
+        }
     }
 }
