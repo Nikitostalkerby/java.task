86245d7513265ab4700c2f7356e7493d2ed790b1
Krivitski Nikita <hax2033@gmail.com>
2015-11-10 14:17:24 +0300

    Change if block to compareTo in Comparator class

--- javase03/src/task03/SortedByYear.java
+++ javase03/src/task03/SortedByYear.java
@@ -12,14 +12,6 @@ public class SortedByYear implements Comparator<Sign> {
         Integer year1 = o1.getbDay().getYear();
         Integer year2 = o2.getbDay().getYear();
 
-        if(year1 > year2) {
-            return 1;
-        }
-        else if(year1 < year2) {
-            return -1;
-        }
-        else {
-            return 0;
-        }
+        return year1.compareTo(year2);
     }
 }
