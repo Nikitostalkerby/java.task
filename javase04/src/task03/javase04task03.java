6334741004cd8cad03fd10b49e1b6886d550b621
Krivitski Nikita <hax2033@gmail.com>
2015-11-10 14:03:22 +0300

    Not Indian man

--- javase04/src/task03/javase04task03.java
+++ javase04/src/task03/javase04task03.java
@@ -1,12 +1,51 @@
 package task03;
 
-import task03.util.Operation;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 /**
  * Created by Vaas on 08.11.2015.
  */
 public class javase04task03 {
     public static void main(String[] args) {
-        Operation operation = new Operation();
+
+        int dividend = 0;
+        int divider = 0;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("\nEnter the dividend: ");
+            dividend = scanner.nextInt();
+
+            System.out.print("\nEnter the divider: ");
+            divider = scanner.nextInt();
+        } catch (NoSuchElementException e) {
+            System.out.println("NoSuchElementException: " + e.toString());
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.toString());
+        }
+
+        if (isLess(dividend, divider)) {
+            findNumbers(dividend, divider);
+        } else {
+            System.out.println("Davai po novoi Miwa.");
+        }
+    }
+
+    public static boolean isLess(int dividend, int divider) {
+        return divider <= dividend;
+    }
+
+    public static void findNumbers(int dividend, int divider) {
+        int quotient = 0;
+        int remainder;
+
+        do {
+            remainder = dividend - divider;
+            dividend = remainder;
+            quotient++;
+        } while (dividend >= divider);
+
+        System.out.print("\nQuotient: " + quotient);
+        System.out.print("\n\nRemainder: " + remainder);
     }
 }
